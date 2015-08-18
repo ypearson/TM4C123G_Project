7a1eda44273f01ca44502ecedc10cd40db72e9d8
Yvan Pearson <ypearson@nklabs.com>
2015-08-18 08:36:04 -0400

    start of cfifo, need to install unity first to test it to death.

--- cfifo.c
+++ cfifo.c
@@ -3,16 +3,26 @@
 
 void cfifo_init(cfifo_t *cf)
 {
-
+	cf->sz = SZ;
+	cf->head = cf->buf[0];
+	cf->tail = cf->buf[SZ-1];
+	cf->cnt = 0;
+	for(i = 0; i < SZ; i++)
+	{
+		cf->buf[i] = 0;
+	}
 }
 
-void cfifo_put(cfifo_t *cf, uint8_t val)
+uint8_t cfifo_put(cfifo_t *cf, uint8_t *val)
 {
 
 }
 
-uint8_t cfifo_get(cfifo_t *cf)
+uint8_t cfifo_get(cfifo_t *cf, uint8_t *val)
 {
+	if(cf->cnt == 0)
+		return 0;
+
 
 }
 
@@ -23,7 +33,7 @@ uint8_t cfifo_peek(cfifo_t *cf)
 
 uint8_t cfifo_cnt(cfifo_t *cf)
 {
-
+	return cf->cnt;
 }
 
 
--- cfifo.h
+++ cfifo.h
@@ -14,8 +14,8 @@ typedef struct
 } cfifo_t;
 
 void    cfifo_init(cfifo_t *cf);
-void    cfifo_put (cfifo_t *cf, uint8_t val);
-uint8_t cfifo_get (cfifo_t *cf);
+uint8_t cfifo_put (cfifo_t *cf, uint8_t *val);
+uint8_t cfifo_get (cfifo_t *cf, uint8_t *val);
 uint8_t cfifo_peek(cfifo_t *cf);
 uint8_t cfifo_cnt (cfifo_t *cf);
 
