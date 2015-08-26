1e44196e0c6e59c5d9ed8ce6beeafc94aa5112c2
Yvan Pearson <ypearson@nklabs.com>
2015-08-26 11:16:40 -0400

    working driver for hih6031.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ fi
 if [ "$1" == "debug" ]; then
  make && (pkill openocd; \
   (openocd --file openocd/share/openocd/scripts/board/ek-tm4c123gxl.cfg > /dev/null 2>&1 &) \
-  && arm-unknown-eabi-gdb -tui $TARGET )
+  && arm-none-eabi-gdb -tui $TARGET )
 else
  make
 fi
