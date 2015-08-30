dd075ebb23395d1bc108d83840dfabf5b311d657
Yvan Pearson <ypearson@nklabs.com>
2015-08-30 16:49:22 -0400

    cfifo minimized. Go back one commit for debug

--- src/cfifo.c
+++ src/cfifo.c
@@ -37,49 +37,31 @@ uint8_t cfifo_put(cfifo_t * cf, uint8_t *val)
     if(cnt < sz)
     {
         if(!head)
-        {
             cf->head = cf->buf;
-        }
 
         *cf->head = *val;
 
         if(head + 1 < buf + sz)
         {
             if(head + 1 == tail)
-            {
                 cf->head = 0;
-                printf("put: head = NULL, cnt = %d\n", cf->cnt+1);
-
-            }
             else
-            {
                 cf->head++;
-                printf("put: head = %x, cnt = %d\n", cf->head - cf->buf, cf->cnt+1);
-            }
         }
         else if(tail == buf)
-        {
             cf->head = 0;
-            printf("put: head = NULL, cnt = %d\n", cf->cnt+1);
-        }
         else
-        {
             cf->head = cf->buf;
-            printf("put: head = %x, cnt = %d\n", cf->head - cf->buf, cf->cnt+1);
-        }
 
         if(!tail)
-        {
             cf->tail = head;
-            printf("put: tail = %x, cnt = %d\n", cf->tail - cf->buf, cf->cnt+1);
-        }
+
         cf->cnt++;
     }
     else
     {
         cf->head = 0;
         res = 1;
-        printf("*put: head = NULL, cnt = %d\n", cf->cnt);
     }
 
     return res;
@@ -100,31 +82,17 @@ uint8_t cfifo_get(cfifo_t *cf, uint8_t *val)
         if(tail + 1 < buf + sz)
         {
             if(tail + 1 == head)
-            {
                 cf->tail = 0;
-                printf("get: tail = NULL, cnt = %d\n", cf->cnt-1);
-            }
             else
-            {
                 cf->tail++;
-                printf("get: tail = %x, cnt = %d\n", cf->tail - cf->buf, cf->cnt-1);
-            }
         }
         else if (head == buf)
-        {
             cf->tail = 0;
-            printf("get: tail = NULL, cnt = %d\n", cf->cnt-1);
-        }
         else
-        {
             cf->tail = buf;
-            printf("get: tail = %x, cnt = %d\n", cf->tail - cf->buf, cf->cnt-1);
-        }
         if(!head)
-        {
             cf->head = tail;
-            printf("get: head = %x, cnt = %d\n", cf->head - cf->buf, cf->cnt-1);
-        }
+
         cf->cnt--;
         res = 0;
     }
@@ -132,7 +100,6 @@ uint8_t cfifo_get(cfifo_t *cf, uint8_t *val)
     {
         cf->tail = 0;
         res = 1;
-        printf("*get: tail = NULL, cnt = %d\n", cf->cnt);
     }
 
     return res;
