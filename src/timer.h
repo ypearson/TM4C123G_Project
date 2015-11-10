4bd31311e82f75be9483e04b63025a0cba50f192
Yvan Pearson <ypearson@nklabs.com>
2015-11-09 20:41:18 -0500

    wip

--- src/timer.h
+++ src/timer.h
@@ -6,13 +6,13 @@
 
 typedef struct
 {
-	uint32_t   cnt1;
-	vars_t   vars[];
+    uint32_t   cnt1;
+    vars_t   vars[];
 } timer_t;
 
 typedef struct
 {
-	uint32_t   tick;
+    uint32_t   tick;
 } clock_t;
 
 
@@ -20,16 +20,16 @@ typedef enum {ONE_SHOT, REPEAT} pmode_t;
 
 typedef struct
 {
-	int     (*start[4])(int argc, char **argv);
-	uint32_t  delay;
-	uint32_t  start;
-	pmode_t   mode;
+    int     (*start[4])(int argc, char **argv);
+    uint32_t  delay;
+    //uint32_t  start;
+    pmode_t   mode;
 } action_group_t;
 
 typedef struct
 {
-	clock_t clock;
-	action_group_t actions[];
+    clock_t clock;
+    action_group_t actions[];
 
 } stimer_t;
 
