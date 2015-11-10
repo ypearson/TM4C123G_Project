a135365b01baebf17f7545d30bcb888834efd721
Yvan Pearson <ypearson@nklabs.com>
2015-11-10 15:33:10 -0500

    wip

--- src/timer.h
+++ src/timer.h
@@ -12,25 +12,26 @@ typedef struct
 
 typedef struct
 {
-    uint32_t   tick;
+    uint32_t   (*now)(void);
 } clock_t;
 
+typedef enum {ONE_SHOT, REPEAT}  pmode_t;
+typedef enum {DISABLED, ENABLED} action_state_t;
 
-typedef enum {ONE_SHOT, REPEAT} pmode_t;
-
-typedef struct
+typedef struct _action_t
 {
-    int     (*start[4])(int argc, char **argv);
-    uint32_t  delay;
-    //uint32_t  start;
-    pmode_t   mode;
-} action_group_t;
+    pmode_t            mode;
+    uint32_t           delay_time;
+    uint32_t           start_time;
+    uint8_t          (*start)(int argc, char **argv);
+    action_state_t     state;
+    struct _action_t  *linked_action;
+} action_t;
 
-typedef struct
+typedef struct _stimer_t;
 {
     clock_t clock;
-    action_group_t actions[];
-
+    action_t actions[];
 } stimer_t;
 
 extern timer_t timer;
@@ -40,6 +41,5 @@ void     timer_init(void);
 void     timer_tick(void);
 uint32_t timer_get_time_now(void);
 uint32_t timer_elasped(uint32_t t0, uint32_t t1);
-
 
 #endif
